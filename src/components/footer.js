a5c9ede8fe79eed53e1ef82c5bea1b74b86f11b9
Hoby R. <hoby.ratefy@protonmail.com>
2025-02-06 16:06:57 +0900

    Fixed hero video sizing for larger screens (4K)

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4506c720ca4594ae379c64f86be6715357f8fe1d39e5bd4da990235357495df
-size 3548
+oid sha256:f146220bbb1f1619a96c2230678b1901f0e1eb22bb258c8440110a3add654f35
+size 3564
