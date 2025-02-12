1b2287b057d4ee9b1689707e33cc2f092c106169
Hoby R. <hoby.ratefy@protonmail.com>
2025-02-12 23:25:02 +0900

    Cleanup code

--- src/pages/coaching.js
+++ src/pages/coaching.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da81cbb24df18f21a5547d8200c566a9de7adeee8990eac7351e969b6034d34a
-size 2446
+oid sha256:fea7460d4d70401a3d7795e8fae196f6a1abafe936d136b9a9293226cdb21c2c
+size 2483
