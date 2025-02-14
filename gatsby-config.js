9694dbf98e80a72f2af6592bd18d1c908a16a0b7
Hoby R. <hoby.ratefy@protonmail.com>
2025-02-14 20:39:58 +0900

    Modified.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d422b9e50b96b8db60e4176e2441006ed3dcb37f6e12b434be62217dbebfea94
-size 591
+oid sha256:a13342e3ef7dc92dce8c8877dbfb9cd03731d53a30f5765f60071287508757e3
+size 568
