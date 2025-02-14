c2677204d0fd026505a27c7c9f7e807a467ff086
Hoby R. <hoby.ratefy@protonmail.com>
2025-02-14 19:50:03 +0900

    Added social media links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c79351811bdc63e2be490128cf61b8f69c8f268cbaec4c36d84d2be9e869df8f
-size 3530
+oid sha256:d75a822515c478fd4aa114679b8a62bc79ea6e12331ac48594c44ba87e7d4621
+size 3801
