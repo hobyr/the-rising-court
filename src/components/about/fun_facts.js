1d557c46dae02c68418bf53c9f71851f1acec0b3
Hoby R. <hoby.ratefy@protonmail.com>
2025-02-15 17:42:18 +0900

    Trying to fix sizing issue with absolute positioning.

--- src/components/about/fun_facts.js
+++ src/components/about/fun_facts.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dad45a28d1dc5e8fad0396b3704513c43208aeaeb0fe431fa809c8965184903f
-size 3645
+oid sha256:066733ca5747274e0c90502ad40a94d4ddf28b5baf6d33a00a05e5332e2e6393
+size 3625
