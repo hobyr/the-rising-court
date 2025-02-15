b2d36d7f5cff70b880138e6cce4c8483c2a0de1e
Hoby R. <hoby.ratefy@protonmail.com>
2025-02-15 14:10:09 +0900

    Figuring out how to deal with positioning absolute and relative blocks.

--- src/components/about/fun_facts.js
+++ src/components/about/fun_facts.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5add48cba190a0ad2bf8005a05a02842905c46bc4ac2b69ce227f5dacc1d8d
-size 3686
+oid sha256:8ee755dfcd9f10d9b38ff42869e83ee39b4ce23f4c908e897de4ec8e8da833e3
+size 3673
