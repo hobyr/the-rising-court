e58e4a91ae32d2a5a46da18f5350e8d87e34e183
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2025-04-14 17:50:41 +0900

    Reduced size of CTA button

--- src/pages/blog/{ContentfulBlogPost.url}.js
+++ src/pages/blog/{ContentfulBlogPost.url}.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c7bdb6da2ddefc0f8bda556bbcc68610f89e24695f7d0eea3a812c23cc8bc73
-size 4852
+oid sha256:1f44c13d271b4bf87f6102fbd7344b40ec14648e3253cd4247fad99e01e954e8
+size 4857
