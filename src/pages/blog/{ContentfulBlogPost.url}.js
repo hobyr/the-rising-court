a4293ce3be73fa2688983d0f83024fa805302d0d
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2025-04-14 17:41:45 +0900

    Separated CTA from content field to create custom CTA button.

--- src/pages/blog/{ContentfulBlogPost.url}.js
+++ src/pages/blog/{ContentfulBlogPost.url}.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:165b7141fc3902b9145d7298106045693973ac506bdacefea109c0d1cc6cb6f9
-size 4378
+oid sha256:8c7bdb6da2ddefc0f8bda556bbcc68610f89e24695f7d0eea3a812c23cc8bc73
+size 4852
