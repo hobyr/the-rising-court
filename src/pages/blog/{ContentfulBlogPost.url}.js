d7512cd38ca38fe085e9af8c28228f814e651674
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2025-04-14 16:49:40 +0900

    Added breadcrumbs to the blog posts.

--- src/pages/blog/{ContentfulBlogPost.url}.js
+++ src/pages/blog/{ContentfulBlogPost.url}.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d599871e0f1695c40a33091d248dbc5c11f0408fe8873734d8e58fab2ca4a7f
-size 4366
+oid sha256:165b7141fc3902b9145d7298106045693973ac506bdacefea109c0d1cc6cb6f9
+size 4378
