1bdfd2a80acf8f2085a5a156ae5d369e0f63ecb2
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2025-05-12 18:05:26 +0900

    Code cleanup

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15a2b58924d05d45ba41feb3aeccaececee69ee8e8a73d3c095f74d0833ecd7b
-size 2864
+oid sha256:83aace6945c83f7bc57ef6a4f5451c4bd2c19707a897a08bbc90fc890da0472c
+size 2865
