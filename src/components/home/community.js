d99cdc2211ffca9d46af24af63826e5c94f4d8a4
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2025-05-12 19:26:35 +0900

    Fixing image issue and button width for mobile.

--- src/components/home/community.js
+++ src/components/home/community.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86599474ffdcd7ffe9ca132ff67c89601e684bfe937345c6a52787e98543752a
-size 2717
+oid sha256:156b8c5a3efe1e88fc738ee6558051a6537aeb3fb54fd37403d672f08025ba81
+size 2739
--- src/components/home/feature.js
+++ src/components/home/feature.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c81bc37ddff1d08081e63591269095ddb676b751d2e87fa20c31b4680a81405
-size 2162
+oid sha256:b89605e3a84b589da8bdcd2e895ef68b9ff2a5508474dd45825bb122c43143e6
+size 2199
