8e7857c7f95b712245831b9cb320ad352a0f7a1d
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2025-09-23 17:56:07 +0900

    Update recommended pricing offer frame

--- src/components/home/ui/price-card.js
+++ src/components/home/ui/price-card.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3053a98cc1c51c9bb5e94679a6d28d68072a4f2247dd5d0d6b0d9486cbd4aaee
-size 2061
+oid sha256:3b81f761e5f00c61a491b10195e531e9ab546e02134eef3019abe87d25ee70c7
+size 2066
