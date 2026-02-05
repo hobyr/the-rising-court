9ded32ba6d1deaa34f2e681e8678c0477f73c84f
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2026-02-05 21:20:33 +0900

    Fix title

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95646bba4b81666b383bc51a5551caa465af42d5f7edbc09479397e10062d46f
-size 2227
+oid sha256:d03bdf3fea3565ea91fe041e71ec80d33583d8ede70fbad2eecca7967667c366
+size 2327
