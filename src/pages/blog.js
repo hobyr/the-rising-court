83aae5316126b1b7b7962bb6e95fdadd9f1755a9
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2026-02-05 20:22:02 +0900

    Fixed title metadata

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1e26306b9e87801cf916d77259052dfb95aa1545a100b2dddd59851d11f7d6e
-size 2103
+oid sha256:95646bba4b81666b383bc51a5551caa465af42d5f7edbc09479397e10062d46f
+size 2227
--- src/pages/blog/{ContentfulBlogPost.url}.js
+++ src/pages/blog/{ContentfulBlogPost.url}.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40a0506ee9baf0f6da069e0a390b7fa4f1b3a22202853d72fad24e5c3fadfebf
-size 4925
+oid sha256:bfcab95edf95ffc93318cd9240298ac7e5b76f942140fb635e7c070bf7b6ddf0
+size 5105
