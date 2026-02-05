5aed6ac42e879418a8a3afc03201023edeebdea9
Hoby Ratefimandimby <hoby.ratefy@protonmail.com>
2026-02-05 16:47:03 +0900

    Improving SEO and adding metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:010e588382e59b37cef51f01c52c369f800751a75cf993e7d2078ce57d2a4f77
-size 603
+oid sha256:a9d1b83e7ed8c2c8fbd0cfd5eb25a43ed18038b3f6c3225d9ff5ff35274265bd
+size 777
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55869761f8e4be79c7e5b84f3fdbd3be7d97690fe1441566e020ae653301bf43
-size 678
+oid sha256:cab64896d94c92c0610a42d7e531892bf04a0a68ece855d0350d99ef79bc9ab0
+size 768
